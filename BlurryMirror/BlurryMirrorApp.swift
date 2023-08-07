c0581ce5ccab885323a5a03d32a8f2a3fd9649ba
Gilles Wittenberg <mail@gilleswittenberg.com>
2023-08-07 14:56:44 +0200

    Initial Commit

---
+++ BlurryMirror/BlurryMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurryMirrorApp.swift
+//  BlurryMirror
+//
+//  Created by Gilles Wittenberg on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlurryMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
